5aca959ccf3d0b4fb31996a02ed5009ffa155943
Enma586 <enmaflore7421@gmail.com>
2026-06-05 19:54:14 -0600

    ADD: Interceptors for transforming responses and handling timeouts have been added to the main application file. The TransformInterceptor is used to modify the response format, while the TimeoutInterceptor ensures that requests do not exceed a specified time limit. Additionally, the global filters and pipes are set up to handle exceptions and validate incoming data, respectively.

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe} from '@nestjs/common';  
 import { HttpExeptionFilter } from './common/filters/http-exception.filter';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor'; 
+import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor'
 async function bootstrap() {
 
   process.env.TZ = 'UTC';
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new TransformInterceptor(), new TimeoutInterceptor());
+ 
   app.useGlobalFilters(new HttpExeptionFilter());
 
   app.useGlobalPipes(
