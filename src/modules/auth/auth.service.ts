0035e573c208c91bcfc23809c3cbc39090171470
Enma586 <enmaflore7421@gmail.com>
2026-06-12 11:44:41 -0600

    add auth service imports

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,6 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import * as bcrypt from 'bcrypt';
+import { LoginDto } from './dto/login.dto';
