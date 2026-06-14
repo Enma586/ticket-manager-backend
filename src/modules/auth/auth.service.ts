ee8ab0c5702ae5d80ea695b437b8dcf1b0b363bc
Enma586 <enmaflore7421@gmail.com>
2026-06-14 17:04:23 -0600

    delete import

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -4,7 +4,7 @@ import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import * as bcrypt from 'bcrypt';
 import { LoginDto } from './dto/login.dto';
-import { isPassportNumber } from 'class-validator';
+
 
 @Injectable()
 export class AuthService{
