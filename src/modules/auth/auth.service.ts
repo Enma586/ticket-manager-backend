956088687bfddd36dc006db3909735488623b74b
Enma586 <enmaflore7421@gmail.com>
2026-06-13 19:14:39 -0600

    ADD: auth.services

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -4,5 +4,70 @@ import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import * as bcrypt from 'bcrypt';
 import { LoginDto } from './dto/login.dto';
+import { isPassportNumber } from 'class-validator';
 
-@Injectable
+@Injectable()
+export class AuthService{
+        constructor(
+        private readonly usersService: UsersService,
+        private readonly jwtService: JwtService,
+        private readonly configService: ConfigService,
+        
+    ){}
+
+    async validateUSer(loginDto: LoginDto){
+        const user = await this.usersService.findByEmail(loginDto.email);
+
+        if(!user.isActive) {
+            throw new UnauthorizedException('Account is suspended')
+        }
+
+        const iSPasswordValid = await bcrypt.compare(loginDto.password, user.password);
+
+        if(!iSPasswordValid){
+            throw new UnauthorizedException('Invalid credentials')
+        }
+
+        const {password, ...result} = user;
+        return result;
+    }
+
+    async login(user: any) {
+    const payload = { email: user.email, sub: user.id, role: user.role };
+
+    const accessToken = this.jwtService.sign(payload, {
+      secret: this.configService.get<string>('JWT_ACCESS_SECRET')!,
+      expiresIn: (this.configService.get<string>('JWT_ACCESS_EXPIRES_IN') || '15m') as any,
+    });
+
+    const refreshToken = this.jwtService.sign(payload, {
+      secret: this.configService.get<string>('JWT_REFRESH_SECRET')!,
+      expiresIn: (this.configService.get<string>('JWT_REFRESH_EXPIRES_IN') || '7d') as any,
+    });
+
+    return { accessToken, refreshToken, user };
+  }
+
+  async refreshToken(token: string) {
+    try {
+      const payload = this.jwtService.verify(token, {
+        secret: this.configService.get<string>('JWT_REFRESH_SECRET')!,
+      });
+
+      const newPayload = { email: payload.email, sub: payload.sub, role: payload.role };
+      
+      const accessToken = this.jwtService.sign(newPayload, {
+        secret: this.configService.get<string>('JWT_ACCESS_SECRET')!,
+        expiresIn: (this.configService.get<string>('JWT_ACCESS_EXPIRES_IN') || '15m') as any,
+      });
+
+      return { accessToken };
+    } catch (error) {
+      throw new UnauthorizedException('Refresh token inválido o expirado');
+    }
+  }
+
+}
+
+
+    
